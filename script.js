cd6fe535a6870faa97fec04bf43fe7c119685313
Marixza Daniel <mdaniel@cristoreyny.org>
2017-02-17 14:50:40 +0000

    project tiny turtle

---
+++ script.js
@@ -0,0 +1,20 @@
+ TinyTurtle.apply(window); 
+ 
+ function triangle() {
+forward(50);
+right(120);
+forward(50);
+right(120);
+forward(50);
+
+    
+    }
+triangle ();
+forward(50);
+right(120);
+forward(50);
+right(120);
+forward(50);
+triangle ();
+
+stamp();
